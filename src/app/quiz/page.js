b17862a2d3371f7d89330fa8cffd38c270cc4097
Peywan Sabir <peywan1999@icloud.com>
2024-06-10 13:17:46 +0200

    changed  localstorage

--- src/app/quiz/page.js
+++ src/app/quiz/page.js
@@ -4,7 +4,6 @@ import Result from '../../components/Result';
 export default function QuizPage() {
     return (
         <div>
-            <h1>Quiz Page</h1>
             <Quiz />
             <Result />
         </div>
